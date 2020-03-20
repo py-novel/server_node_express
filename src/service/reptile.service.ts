4d30af54f76cc83a59fbcc34fba45416ba49cecb
dkvirus <3548310748@qq.com>
2020-03-20 09:28:52 +0800

    fix: services 目录改名为 service

---
+++ src/service/reptile.service.ts
@@ -0,0 +1,21 @@
+
+
+export default {
+
+    reptileNovelList() {
+
+    },
+
+    reptileNovelIntro() {
+
+    },
+
+    reptileChapterList() {
+
+    },
+
+    reptileNovelContent() {
+
+    }
+
+}
