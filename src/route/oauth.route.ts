d8175ec91f0b007180fccc107c0360c671ebca98
dkvirus <3548310748@qq.com>
2020-03-22 09:26:06 +0800

    fix: 注释email和mobile服务,保证没有redis也能正常启动项目

--- src/route/oauth.route.ts
+++ src/route/oauth.route.ts
@@ -4,7 +4,7 @@ import jwt from '@apacejs/jwt'
 import { tokenExpiresIn } from '../config'
 import User from '../entity/User.entity'
 import userService from '../service/user.service'
-import mobileService from '../service/mobile.service'
+// import mobileService from '../service/mobile.service'
 import AdminResponse from '../util/AdminResponse'
 
 const log = debug('src/route/oauth')
@@ -73,96 +73,96 @@ export default {
      * 注册，新增用户
      */
     signup: async function (req: Request, res: Response) {
-        const { username, password, vcode } = req.body
-        log(`signup() param of username: ${username} password: ${password} vcode: ${vcode}`)
+        // const { username, password, vcode } = req.body
+        // log(`signup() param of username: ${username} password: ${password} vcode: ${vcode}`)
 
-        // 校验参数是否必填以及格式是否正确
-        if (!username) {
-            return res.json(AdminResponse.failure('手机号不能为空'))
-        } else if (String(username).length !== 11 || !(/^1[34578]\d{9}$/.test(String(username)))) {
-            return res.json(AdminResponse.failure('手机号格式不正确'))
-        }
+        // // 校验参数是否必填以及格式是否正确
+        // if (!username) {
+        //     return res.json(AdminResponse.failure('手机号不能为空'))
+        // } else if (String(username).length !== 11 || !(/^1[34578]\d{9}$/.test(String(username)))) {
+        //     return res.json(AdminResponse.failure('手机号格式不正确'))
+        // }
 
-        if (!password) {
-            return res.json(AdminResponse.failure('密码不能为空'))
-        }
+        // if (!password) {
+        //     return res.json(AdminResponse.failure('密码不能为空'))
+        // }
 
-        if (!vcode) {
-            return res.json(AdminResponse.failure('短信验证码不能为空'))
-        }
+        // if (!vcode) {
+        //     return res.json(AdminResponse.failure('短信验证码不能为空'))
+        // }
 
-        try {
-            // 根据手机号去用户表拿数据，手机号必须没有注册过的，才能进行注册
-            const user = await userService.findOneByUsername(username)
-            if (user) {
-                return res.json(AdminResponse.failure('手机号已注册，请直接登录'))
-            }
+        // try {
+        //     // 根据手机号去用户表拿数据，手机号必须没有注册过的，才能进行注册
+        //     const user = await userService.findOneByUsername(username)
+        //     if (user) {
+        //         return res.json(AdminResponse.failure('手机号已注册，请直接登录'))
+        //     }
 
-            // 校验验证码是否正确
-            const vcodeResult = await mobileService.validateVcode({ vcode, mobile: username })
-            // 验证码不正确，返回提示文字
-            if (vcodeResult.code !== '0000') {
-                return res.json(vcodeResult)
-            }
+        //     // 校验验证码是否正确
+        //     const vcodeResult = await mobileService.validateVcode({ vcode, mobile: username })
+        //     // 验证码不正确，返回提示文字
+        //     if (vcodeResult.code !== '0000') {
+        //         return res.json(vcodeResult)
+        //     }
 
-            // 验证码正确，新增一条用户信息
-            let newUser = new User()
-            newUser.username = username
-            newUser.password = password
-            newUser.clientType = 'MOBILE'
-            newUser = await userService.saveUser(newUser)
-            res.json(AdminResponse.success({ userId: newUser.id }, '注册成功'))
-        } catch (e) {
-            log(`signup() 注册失败: ${e.message}`)
-            res.json(AdminResponse.failure('注册失败'))
-        }
+        //     // 验证码正确，新增一条用户信息
+        //     let newUser = new User()
+        //     newUser.username = username
+        //     newUser.password = password
+        //     newUser.clientType = 'MOBILE'
+        //     newUser = await userService.saveUser(newUser)
+        //     res.json(AdminResponse.success({ userId: newUser.id }, '注册成功'))
+        // } catch (e) {
+        //     log(`signup() 注册失败: ${e.message}`)
+        //     res.json(AdminResponse.failure('注册失败'))
+        // }
     },
 
     /**
      * 重置密码，修改用户
      */
     resetpw: async function (req: Request, res: Response) {
-        const { username, password, vcode } = req.body
-        log(`resetpw() param of username: ${username} password: ${password} vcode: ${vcode}`)
+        // const { username, password, vcode } = req.body
+        // log(`resetpw() param of username: ${username} password: ${password} vcode: ${vcode}`)
 
-        // 校验参数是否必填以及格式是否正确
-        if (!username) {
-            return res.json(AdminResponse.failure('手机号不能为空'))
-        } else if (String(username).length !== 11 || !(/^1[34578]\d{9}$/.test(String(username)))) {
-            return res.json(AdminResponse.failure('手机号格式不正确'))
-        }
+        // // 校验参数是否必填以及格式是否正确
+        // if (!username) {
+        //     return res.json(AdminResponse.failure('手机号不能为空'))
+        // } else if (String(username).length !== 11 || !(/^1[34578]\d{9}$/.test(String(username)))) {
+        //     return res.json(AdminResponse.failure('手机号格式不正确'))
+        // }
 
-        if (!password) {
-            return res.json(AdminResponse.failure('密码不能为空'))
-        }
+        // if (!password) {
+        //     return res.json(AdminResponse.failure('密码不能为空'))
+        // }
 
-        if (!vcode) {
-            return res.json(AdminResponse.failure('短信验证码不能为空'))
-        }
+        // if (!vcode) {
+        //     return res.json(AdminResponse.failure('短信验证码不能为空'))
+        // }
 
-        try {
-            // 根据手机号去用户表拿数据，重置密码要求手机号必须已经注册过，才能改密码
-            const user = await userService.findOneByUsername(username)
+        // try {
+        //     // 根据手机号去用户表拿数据，重置密码要求手机号必须已经注册过，才能改密码
+        //     const user = await userService.findOneByUsername(username)
 
-            if (!user) {
-                return res.json(AdminResponse.failure('手机号未注册，请先注册新用户'))
-            }
+        //     if (!user) {
+        //         return res.json(AdminResponse.failure('手机号未注册，请先注册新用户'))
+        //     }
 
-            // 校验验证码是否正确
-            const vcodeResult = await mobileService.validateVcode({ vcode, mobile: username })
-            // 验证码不正确，返回提示文字
-            if (vcodeResult.code !== '0000') {
-                return res.json(vcodeResult)
-            }
+        //     // 校验验证码是否正确
+        //     const vcodeResult = await mobileService.validateVcode({ vcode, mobile: username })
+        //     // 验证码不正确，返回提示文字
+        //     if (vcodeResult.code !== '0000') {
+        //         return res.json(vcodeResult)
+        //     }
 
-            // 验证码输入正确，修改用户密码
-            user.password = password
-            await userService.saveUser(user)
-            res.json(AdminResponse.success('重置密码成功'))
-        } catch (e) {
-            log(`resetpw() 重置密码失败: ${e.message}`)
-            res.json(AdminResponse.failure('重置密码失败'))
-        }
+        //     // 验证码输入正确，修改用户密码
+        //     user.password = password
+        //     await userService.saveUser(user)
+        //     res.json(AdminResponse.success('重置密码成功'))
+        // } catch (e) {
+        //     log(`resetpw() 重置密码失败: ${e.message}`)
+        //     res.json(AdminResponse.failure('重置密码失败'))
+        // }
     },
 
     /**
@@ -170,40 +170,40 @@ export default {
      * type: 'signup' | 'resetpw'
      */
     sendVcode: async function (req: Request, res: Response) {
-        const { username, type } = req.body
-        log(`sendVcode() param of username: ${username} type: ${type}`)
+        // const { username, type } = req.body
+        // log(`sendVcode() param of username: ${username} type: ${type}`)
 
-        // 校验参数必填
-        if (!username) {
-            return res.json(AdminResponse.failure('手机号必填'))
-        } else if (String(username).length !== 11 || !(/^1[34578]\d{9}$/.test(String(username)))) {
-            return res.json(AdminResponse.failure('手机号格式不正确'))
-        }
+        // // 校验参数必填
+        // if (!username) {
+        //     return res.json(AdminResponse.failure('手机号必填'))
+        // } else if (String(username).length !== 11 || !(/^1[34578]\d{9}$/.test(String(username)))) {
+        //     return res.json(AdminResponse.failure('手机号格式不正确'))
+        // }
 
-        if (!type) {
-            return res.json(AdminResponse.failure('功能类型不能为空'))
-        }
+        // if (!type) {
+        //     return res.json(AdminResponse.failure('功能类型不能为空'))
+        // }
 
-        try {
-            // 根据手机号去用户表拿数据
-            const user = await userService.findOneByUsername(username)
+        // try {
+        //     // 根据手机号去用户表拿数据
+        //     const user = await userService.findOneByUsername(username)
 
-            // type=“signup” 注册时要求之前没有往用户表插过手机号
-            if (type === 'signup' && user) {
-                return res.json(AdminResponse.failure('手机号已注册，请直接登录'))
-            }
+        //     // type=“signup” 注册时要求之前没有往用户表插过手机号
+        //     if (type === 'signup' && user) {
+        //         return res.json(AdminResponse.failure('手机号已注册，请直接登录'))
+        //     }
 
-            // type="resetpw" 重置密码要求之前已经往用户表插过手机号
-            if (type === 'resetpw' && !user) {
-                return res.json(AdminResponse.failure('手机号未注册，请先注册新用户'))
-            }
+        //     // type="resetpw" 重置密码要求之前已经往用户表插过手机号
+        //     if (type === 'resetpw' && !user) {
+        //         return res.json(AdminResponse.failure('手机号未注册，请先注册新用户'))
+        //     }
 
-            // 发送短信验证码，返回响应
-            const vcodeResult = await mobileService.sendVcode({ mobile: username })
-            res.json(vcodeResult)
-        } catch (e) {
-            log(`sendVcode() 发送验证码失败: ${e.message}`)
-            res.json(AdminResponse.failure('发送验证码失败'))
-        }
+        //     // 发送短信验证码，返回响应
+        //     const vcodeResult = await mobileService.sendVcode({ mobile: username })
+        //     res.json(vcodeResult)
+        // } catch (e) {
+        //     log(`sendVcode() 发送验证码失败: ${e.message}`)
+        //     res.json(AdminResponse.failure('发送验证码失败'))
+        // }
     },
 }
